3fa6e6d5b9a7a07bc8b80c659fbd7e44aa3149b3
David Monaghan <github@allstruck.com>
2013-06-01 11:50:44 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityIdeas::Application.config.secret_token = '4749a58174b07b69da95717a21cf6b266243f7369c75c2e5c5221d85ec1ebfdc876786631e80585e62c9375e9da38fa57d20844fe414e63301e77713ab69d777'
