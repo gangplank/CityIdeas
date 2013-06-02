d213fecb6d74dfc5aea9414108e7965037fb4320
Derek Neighbors <derek@integrumtech.com>
2013-06-02 16:37:59 -0700

    add vote_count for vote caching.  update cache when made. sort votes by order.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,13 @@ class Idea < ActiveRecord::Base
   acts_as_votable
   acts_as_commentable
   
-  attr_accessible :description, :title, :status_id, :category_id
+  attr_accessible :description, :title, :status_id, :category_id, :vote_score
   belongs_to :status
   belongs_to :category
+  
+  def update_vote_score
+    self.vote_score = self.upvotes.size - self.downvotes.size
+    self.save!
+  end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602202424) do
+ActiveRecord::Schema.define(:version => 20130602230731) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130602202424) do
     t.datetime "updated_at",  :null => false
     t.integer  "status_id"
     t.integer  "category_id"
+    t.integer  "vote_score"
   end
 
   create_table "statuses", :force => true do |t|
