63ea900e92de16f7987daa184161014f1c0a6f8e
Peter J. Hart <peter@peterjhart.com>
2013-06-02 16:56:17 -0700

    Added color and icon to statuses.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602202424) do
+ActiveRecord::Schema.define(:version => 20130602233134) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(:version => 20130602202424) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "color"
+    t.string   "icon"
   end
 
   create_table "users", :force => true do |t|
