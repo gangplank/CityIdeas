3fea2aa16d9ba290de0b7d86f75b823b17e56aaf
Derek Neighbors <derek@integrumtech.com>
2013-06-02 12:37:28 -0700

    fixed seeds file.  added dropdown for neighborhood(category) to idea.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,18 @@
 
 User.create!({:email => "admin@cityideas.com", :password => "password", :password_confirmation => "password" })
 
+
+categories = Category.create([{ name: 'City Wide' }, 
+                              { name: '85224 - North Central Chandler' }, 
+                              { name: '85225 - Downtown Chandler' }, 
+                              { name: '85226 - South West Chandler' }, 
+                              { name: '85244 - Government Complex' }, 
+                              { name: '85246 - North West Chandler' }, 
+                              { name: '85248 - Ocotillo / Sun Lakes' }, 
+                              { name: '85249 - South Chandler' }, 
+                              { name: '85286 - Central Chandler' }])
+
+
 statuses = Status.create([{ name: 'under review' }, 
                         { name: 'planned' }, 
                         { name: 'started' }, 
@@ -16,8 +28,10 @@ statuses = Status.create([{ name: 'under review' },
                         
 Idea.create(title: 'My New Idea',
             description: 'This idea just came to me.  Like it?',
-            status_id: statuses.first.id)
+            status_id: statuses.first.id,
+            category_id: categories.first.id)
 
 Idea.create(title: 'My Great Idea',
             description: 'I know they will accept this one.',
-            status_id: statuses[1].id)
+            status_id: statuses[1].id,
+            category_id: categories[1].id)
