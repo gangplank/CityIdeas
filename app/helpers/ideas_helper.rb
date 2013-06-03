44f4bda2e90301e9434878fe181fffd07726ea40
David Monaghan <github@allstruck.com>
2013-06-02 22:07:50 -0700

    Added vote direction indicator via color

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -2,9 +2,9 @@ module IdeasHelper
 	def render_voted_class (user, idea)
 		return "" if !user
 		if user.voted_as_when_voted_for(idea)
-			"votedUp"
+			"userupvoted"
 		else
-			"votedDown"
+			"userdownvoted"
 		end
 	end
 end
